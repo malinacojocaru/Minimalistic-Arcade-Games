0f1aa50eddd93bc5eafdf9420054309b49756071
Diana Velciu <dianamariavelciu@yahoo.com>
2025-01-04 18:45:11 +0200

    add pipes

--- flappy_bird_game/flappy_bird.py
+++ flappy_bird_game/flappy_bird.py
@@ -1,5 +1,6 @@
 
 import pygame
+import random
 from sys import exit
 
 pygame.init()
@@ -48,11 +49,29 @@ def quit_game():
             pygame.quit()
             exit()
 
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, pipe_type):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pipe_type
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.passed = False
+
+    def update(self):
+        self.rect.x = self.rect.x - speed
+        if self.rect.x <= -WIDTH:
+            self.kill()
+
 def main():
     x = 0
     y = 520
+    timer = 0
+
     ground = pygame.sprite.Group()
     ground.add(Ground(x, y))
+
+    pipes = pygame.sprite.Group()
 
     bird = pygame.sprite.GroupSingle()
     bird.add(Bird())
@@ -67,10 +86,23 @@ def main():
             ground.add(Ground(WIDTH, y))
 
         bird.draw(window)
+        pipes.draw(window)
         ground.draw(window)
+
 
         bird.update()
         ground.update()
+        pipes.update()
+
+        if timer <= 0:
+            x_top = 550
+            x_bottom = 550
+            y_top = random.randint(-650, -500)
+            y_bottom = y_top + random.randint(130, 170) + pipe_bottom.get_height()
+            pipes.add(Pipe(x_top, y_top, pipe_top))
+            pipes.add(Pipe(x_bottom, y_bottom, pipe_bottom))
+            timer = random.randint(200, 250)
+        timer = timer - 1
 
         pygame.display.update()
 
