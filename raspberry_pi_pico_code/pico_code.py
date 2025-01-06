695aec161c10f9817081b7209b95a6ca39a9d0e6
malinacojocaru <malinacojocaru4@gmail.com>
2025-01-06 20:43:53 +0200

    added pico code

---
+++ raspberry_pi_pico_code/pico_code.py
@@ -0,0 +1,9 @@
+from machine import ADC, Pin
+import time
+
+adc_pins = [ADC(Pin(26)), ADC(Pin(27)), ADC(Pin(28))]
+
+while True:
+    readings = [adc.read_u16() for adc in adc_pins]
+    print(readings)
+    time.sleep(0.1)
